ec85e643bfaafbe22d1da6280fe26b7d33ea09f8
Mark A Green <mark.green@liverpool.ac.uk>
2021-02-06 14:57:22 +0000

    Update spatial_model_uptake.R

--- scripts/spatial_model_uptake.R
+++ scripts/spatial_model_uptake.R
@@ -17,14 +17,17 @@ library(sf)
 #  load clean test data
 load(normalizePath("other_data/clean_testing.Rdata"))
 
-# Subset liverpool LA and lft tests
-lft_tests=testing[lft==1 &lad11cd=="E08000012"]
+# Drop all tests where age <= 5 (there should not be any, so assume erroneous data)
+testing <- testing[age>5]
+
+# Subset liverpool LA and lft tests, for time period of analysis
+lft_tests <- testing[lft==1 & lad11cd=="E08000012" & t_date >= "2020-11-06 00:00:00" & t_date < "2021-02-01 00:00:00"]
 rm(testing)
 
 # Subset data into time periods for analysis
 lft_tests_pilot <- lft_tests[lft_tests$t_date >= "2020-11-06 00:00:00" & lft_tests$t_date < "2020-12-03 00:00:00"] # Pilot period
 lft_tests_xmas <- lft_tests[lft_tests$t_date >= "2020-12-03 00:00:00" & lft_tests$t_date < "2021-01-06 00:00:00"] # Christmas period
-lft_tests_lockdown <- lft_tests[lft_tests$t_date >= "2021-01-06 00:00:00"] # Lockdown
+lft_tests_lockdown <- lft_tests[lft_tests$t_date >= "2021-01-06 00:00:00" & lft_tests$t_date < "2021-02-01 00:00:00"] # Lockdown
 
 
 ### 2. Descriptives for paper  ###
@@ -38,7 +41,7 @@ table1 # Print
 write.csv(table1, "./output/descriptives_table_uptake.csv") # Save
 
 # Tidy
-rm(lft_tests, lft_tests_pilot, lft_tests_xmas, lft_tests_lockdown)
+rm(lft_tests, lft_tests_pilot, lft_tests_xmas, lft_tests_lockdown, table1)
 
 ### 3. Setting up the spatial model ###
 
@@ -65,8 +68,13 @@ g <- inla.read.graph(filename = "./other_data/liverpool.graph") # Load graph in
 # Join on data onto shapefile
 model_data <- merge(lsoas, lsoa_lft_all, by.x = "code", by.y = "lsoa11", all.x = TRUE) 
 
+# Standardise/centre continuous variables
+model_data$zimd_score <- scale(model_data$imd_score, center = TRUE, scale = TRUE)
+model_data$zwalkDistAvg <- scale(model_data$walkDistAvg, center = TRUE, scale = TRUE)
+model_data$zprop_students <- scale(model_data$prop_students, center = TRUE, scale = TRUE)
+
 # Define formula
-formula1 <- lft ~ imd_score + prop_students + ch_prop + walkDistAvg + factor(grp_label) + f(idx, model = "bym", graph = g)
+formula1 <- lft ~ zimd_score + zprop_students + ch_binary + zwalkDistAvg + factor(grp_label) + f(idx, model = "bym", graph = g)
 
 # Fit model
 model1 <- inla(formula1, family="poisson", data=model_data, E = model_data$exp_num, control.predictor = list(compute = TRUE))
@@ -91,7 +99,7 @@ plot_m1 <- ggplot(model_data) + # Plot
                        values = scales::rescale(c(0.3,0.9,1,1.1,6.9)), # Define values for colours
                        limits = c(0.3, 6.9)) +
   labs(fill = "Relative Risk") +
-  ggtitle("6th Nov-13th Jan") +
+  ggtitle("6th Nov-31st Jan") +
   xlab("Longitude") +
   ylab("Latitude")
 plot_m1
@@ -109,8 +117,13 @@ plot_m1
 # Join on data onto shapefile
 model_data <- merge(lsoas, lsoa_lft_pilot, by.x = "code", by.y = "lsoa11", all.x = TRUE) 
 
+# Standardise/centre continuous variables
+model_data$zimd_score <- scale(model_data$imd_score, center = TRUE, scale = TRUE)
+model_data$zwalkDistAvg <- scale(model_data$walkDistAvg, center = TRUE, scale = TRUE)
+model_data$zprop_students <- scale(model_data$prop_students, center = TRUE, scale = TRUE)
+
 # Define formula
-formula1 <- lft ~ imd_score + prop_students + ch_prop + walkDistAvg + factor(grp_label) + f(idx, model = "bym", graph = g)
+formula1 <- lft ~ zimd_score + zprop_students + ch_binary + zwalkDistAvg + factor(grp_label) + f(idx, model = "bym", graph = g)
 
 # Fit model
 model2 <- inla(formula1, family="poisson", data=model_data, E = model_data$exp_num, control.predictor = list(compute = TRUE))
@@ -136,8 +149,13 @@ plot_m2
 # Join on data onto shapefile
 model_data <- merge(lsoas, lsoa_lft_xmas, by.x = "code", by.y = "lsoa11", all.x = TRUE) 
 
+# Standardise/centre continuous variables
+model_data$zimd_score <- scale(model_data$imd_score, center = TRUE, scale = TRUE)
+model_data$zdistance <- scale(model_data$distance, center = TRUE, scale = TRUE)
+model_data$zprop_students <- scale(model_data$prop_students, center = TRUE, scale = TRUE)
+
 # Define formula
-formula1 <- lft ~ imd_score + prop_students + ch_prop + distance + factor(grp_label) + f(idx, model = "bym", graph = g)
+formula1 <- lft ~ zimd_score + zprop_students + ch_binary + zdistance + factor(grp_label) + f(idx, model = "bym", graph = g)
 
 # Fit model
 model3 <- inla(formula1, family="poisson", data=model_data, E = model_data$exp_num, control.predictor = list(compute = TRUE))
@@ -163,8 +181,13 @@ plot_m3
 # Join on data onto shapefile
 model_data <- merge(lsoas, lsoa_lft_lockdown, by.x = "code", by.y = "lsoa11", all.x = TRUE) 
 
+# Standardise/centre continuous variables
+model_data$zimd_score <- scale(model_data$imd_score, center = TRUE, scale = TRUE)
+model_data$zdistance <- scale(model_data$distance, center = TRUE, scale = TRUE)
+model_data$zprop_students <- scale(model_data$prop_students, center = TRUE, scale = TRUE)
+
 # Define formula
-formula1 <- lft ~ imd_score + prop_students + ch_prop + distance + factor(grp_label) + f(idx, model = "bym", graph = g)
+formula1 <- lft ~ zimd_score + zprop_students + ch_binary + zdistance + factor(grp_label) + f(idx, model = "bym", graph = g)
 
 # Fit model
 model4 <- inla(formula1, family="poisson", data=model_data, E = model_data$exp_num, control.predictor = list(compute = TRUE))
@@ -179,7 +202,7 @@ plot_m4 <- ggplot(model_data) + # Plot
                        values = scales::rescale(c(0.3,0.9,1,1.1,6.9)), # Define values for colours
                        limits = c(0.3, 6.9)) +
   labs(fill = "Relative Risk") +
-  ggtitle("6th-13th Jan") +
+  ggtitle("6th-31st Jan") +
   xlab("Longitude") +
   ylab("Latitude")
 plot_m4
@@ -192,28 +215,32 @@ plot_m4
 options(scipen=999)
 
 # Store FE effects (raw)
-fe_1 <- as.data.frame(model1$summary.fixed) # Model 1
-fe_1 <- fe_1[c("mean", "0.025quant", "0.975quant")] # Subset vars
-colnames(fe_1) <- paste("m1", colnames(fe_1), sep = "_") # Rename
+fe_1 <- setDT(model1$summary.fixed, keep.rownames = "variable") # Model 1
+fe_1 <- fe_1[2:14,] # Drop intercept
+fe_1 <- rbind(fe_1, data.table("variable" = "e-Veterans", "mean" = 0, "sd" = 0, "0.025quant" = 0, "0.5quant" = 0, "0.975quant" = 0, "mode" = 0, "kld" = 0)) # Add in reference category
+fe_1$model <- "6th Nov - 31st Jan" # Add model label
 
-fe_2 <- as.data.frame(model2$summary.fixed) # Model 2
-fe_2 <- fe_2[c("mean", "0.025quant", "0.975quant")]
-colnames(fe_2) <- paste("m2", colnames(fe_2), sep = "_") # Rename
+fe_2 <- setDT(model2$summary.fixed, keep.rownames = "variable") # Model 2
+fe_2 <- fe_2[2:14,] # Drop intercept
+fe_2 <- rbind(fe_2, data.table("variable" = "e-Veterans", "mean" = 0, "sd" = 0, "0.025quant" = 0, "0.5quant" = 0, "0.975quant" = 0, "mode" = 0, "kld" = 0)) # Add in reference category
+fe_2$model <- "6th Nov - 2nd Dec" # Add model label
 
-fe_3 <- as.data.frame(model3$summary.fixed) # Model 3
-fe_3 <- fe_3[c("mean", "0.025quant", "0.975quant")]
-colnames(fe_3) <- paste("m3", colnames(fe_3), sep = "_") # Rename
+fe_3 <- setDT(model3$summary.fixed, keep.rownames = "variable") # Model 3
+fe_3 <- fe_3[2:14,] # Drop intercept
+fe_3 <- rbind(fe_3, data.table("variable" = "e-Veterans", "mean" = 0, "sd" = 0, "0.025quant" = 0, "0.5quant" = 0, "0.975quant" = 0, "mode" = 0, "kld" = 0)) # Add in reference category
+fe_3$model <- "3rd Dec - 5th Jan" # Add model label
 
-fe_4 <- as.data.frame(model4$summary.fixed) # Model 4
-fe_4 <- fe_4[c("mean", "0.025quant", "0.975quant")]
-colnames(fe_4) <- paste("m4", colnames(fe_4), sep = "_") # Rename
+fe_4 <- setDT(model4$summary.fixed, keep.rownames = "variable") # Model 4
+fe_4 <- fe_4[2:14,] # Drop intercept
+fe_4 <- rbind(fe_4, data.table("variable" = "e-Veterans", "mean" = 0, "sd" = 0, "0.025quant" = 0, "0.5quant" = 0, "0.975quant" = 0, "mode" = 0, "kld" = 0)) # Add in reference category
+fe_4$model <- "6th Jan - 31st Jan" # Add model label
 
-fe_table <- cbind(fe_1, fe_2, fe_3, fe_4) # Combine
+fe_table <- rbind(fe_1, fe_2, fe_3, fe_4) # Combine
 fe_table # Print
 write.csv(fe_table, "./output/fe_uptake_model_raw.csv") # Save
 
-fe_table <- exp(fe_table) # Calculate relative risk
-write.csv(fe_table, "./output/fe_uptake_model_rr.csv") # Save
+fe_table_rr <- cbind(fe_table[,c(9,1)], exp(fe_table[,2:7])) # Calculate relative risk
+write.csv(fe_table_rr, "./output/fe_uptake_model_rr.csv") # Save
 rm(fe_1, fe_2, fe_3, fe_4) # Tidy
 
 # Store RE effects
@@ -238,6 +265,23 @@ re_table # Print
 write.csv(re_table, "./output/re_uptake_models.csv") # Save
 rm(re_1, re_2, re_3, re_4) # Tidy
 
+# Store model fit
+mlik_1 <- as.data.frame(model1$mlik) # Model1
+names(mlik_1)[names(mlik_1) == "V1"] <- "model1" # Rename column
+
+mlik_2 <- as.data.frame(model2$mlik) # Model2
+names(mlik_2)[names(mlik_2) == "V1"] <- "model2" # Rename column
+
+mlik_3 <- as.data.frame(model3$mlik) # Model3
+names(mlik_3)[names(mlik_3) == "V1"] <- "model3" # Rename column
+
+mlik_4 <- as.data.frame(model4$mlik) # Model4
+names(mlik_4)[names(mlik_4) == "V1"] <- "model4" # Rename column
+
+mlik_table <- cbind(mlik_1, mlik_2, mlik_3, mlik_4) # Combine # Join together
+write.csv(mlik_table, "./output/mlik_uptake_model.csv") # Save
+rm(mlik_1, mlik_2, mlik_3, mlik_4) # Tidy
+
 
 ### 9. Put plots together ###
 
@@ -260,30 +304,36 @@ lsoa_lft_all <- merge(lsoa_lft_all, ofcom, by.x = "lsoa11", by.y = "LSOA11CD", a
 # Join on data onto shapefile
 model_data <- merge(lsoas, lsoa_lft_all, by.x = "code", by.y = "lsoa11", all.x = TRUE) 
 
-# Median data usage (GB)
+# Median data usage (GB) #
+
+# Standardise/centre continuous variables
+model_data$zimd_score <- scale(model_data$imd_score, center = TRUE, scale = TRUE)
+model_data$zwalkDistAvg <- scale(model_data$walkDistAvg, center = TRUE, scale = TRUE)
+model_data$zprop_students <- scale(model_data$prop_students, center = TRUE, scale = TRUE)
+model_data$zmedian_data <- scale(model_data$Median.data.usage..GB., center = TRUE, scale = TRUE)
 
 # Define formula
-formula1 <- lft ~ imd_score + prop_students + ch_prop + walkDistAvg + Median.data.usage..GB. + f(idx, model = "bym", graph = g)
+formula1 <- lft ~ zimd_score + zprop_students + ch_binary + zwalkDistAvg + zmedian_data + f(idx, model = "bym", graph = g)
 
 # Fit model
 model_5 <- inla(formula1, family="poisson", data=model_data, E = model_data$exp_num, control.predictor = list(compute = TRUE))
 summary(model_5) # Print results
 
-# Interaction effects
+# Interaction effects #
 
-# IUC
+# 1. IUC
 
 # Define formula
-formula1 <- lft ~ imd_score + prop_students + ch_prop + walkDistAvg + factor(grp_label) + factor(grp_label)*imd_score + f(idx, model = "bym", graph = g)
+formula1 <- lft ~ zimd_score + zprop_students + ch_binary + zwalkDistAvg + factor(grp_label) + factor(grp_label)*zimd_score + f(idx, model = "bym", graph = g)
 
 # Fit model
 model_6 <- inla(formula1, family="poisson", data=model_data, E = model_data$exp_num, control.predictor = list(compute = TRUE))
 summary(model_6) # Print results
 
-# Median data
+# 2. Median data downloads
 
 # Define formula
-formula1 <- lft ~ imd_score + prop_students + ch_prop + walkDistAvg + Median.data.usage..GB. + Median.data.usage..GB.*imd_score + f(idx, model = "bym", graph = g)
+formula1 <- lft ~ zimd_score + zprop_students + ch_binary + zwalkDistAvg + zmedian_data + zmedian_data*zimd_score + f(idx, model = "bym", graph = g)
 
 # Fit model
 model_7 <- inla(formula1, family="poisson", data=model_data, E = model_data$exp_num, control.predictor = list(compute = TRUE))
@@ -311,6 +361,21 @@ write.csv(fe_table2, "./output/fe_uptake_sensitivity_raw.csv") # Save
 fe_table2 <- exp(fe_table2) # Calculate relative risk
 write.csv(fe_table2, "./output/fe_uptake_sensitivity_rr.csv") # Save
 rm(fe_5, fe_6, fe_7) # Tidy
+
+# Store model fit
+mlik_5 <- as.data.frame(model_5$mlik) # Model5
+names(mlik_5)[names(mlik_5) == "V1"] <- "model5" # Rename column
+
+mlik_6 <- as.data.frame(model_6$mlik) # Model2
+names(mlik_6)[names(mlik_6) == "V1"] <- "model6" # Rename column
+
+mlik_7 <- as.data.frame(model_7$mlik) # Model3
+names(mlik_7)[names(mlik_7) == "V1"] <- "model7" # Rename column
+
+mlik_table <- cbind(mlik_5, mlik_6, mlik_7) # Combine # Join together
+write.csv(mlik_table, "./output/mlik_uptake_sensitivity_model.csv") # Save
+rm(mlik_5, mlik_6, mlik_7) # Tidy
+
 
 # Tidy
 rm(list=ls()) # Get rid of all data/objects
